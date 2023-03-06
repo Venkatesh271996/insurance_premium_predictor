f952f26558ac843446e7a9c1689e4b4f6c425e55
Venkatesh271996 <109842796+Venkatesh271996@users.noreply.github.com>
2023-03-06 20:10:08 +0530

    Add files via upload

---
+++ insurance_premium_predictor.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("Insurance premium prediction app")
+st.header("Hi People")
+st.subheader("Welcome to insurance premium prediction app")
+# st.code("for writing code block")
+# st.write(" for the code execution")\
